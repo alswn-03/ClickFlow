9e6577d3992cf291ea52f4570710877d77955a3d
alswn-03 <soojooyong@gmail.com>
2026-08-29 18:57:23 +0900

    chore: producer.ts 주석 정리 및 수정

--- server/src/kafka/producer.ts
+++ server/src/kafka/producer.ts
@@ -1,21 +1,27 @@
+/* 
+⭐️ Express 서버 ↔ Kafka 브로커 : Kafka producer를 통해 발생한 이벤트를 Kafka 브로커에 publish하는 로직
+*/
+
+import { kafka } from "./client.js";
 // import { Kafka } from "kafkajs";
 
 // // Kafka 클라이언트 생성
-// const kafka = new Kafka({
+// export const kafka = new Kafka({
 //   clientId: "clickflow-api", // Kafka 브로커가 '이 요청이 어디서 온거지?'를 구분하기 위해 사용하는 이름표
 //   brokers: ["localhost:9092"], // 실제 연결 - Kafka 브로커의 주소
 // });
 
-import { kafka } from "./client.js";
-
-// producer(인스턴스) 생성 + kafka 브로커와 연결
+// 0. producer(인스턴스) 생성
 const producer = kafka.producer();
 
+// 1. Express 서버(app.ts) -> producer.ts : connectProducer() 호출
+//    producer(인스턴스) -> Kafka 브로커 : (TCP) 연결
 export async function connectProducer() {
   await producer.connect(); // producer(인스턴스)를 실제 kafka 브로커와 (TCP) 연결
 }
 
-// 메시지 발생 : 이벤트를 Kafka 브로커에 publish(전송)하는 함수
+// 📩 2. Express 서버(routes.ts) -> producer.ts : publishEvent() 호출
+//    producer(인스턴스) -> Kafka 브로커 : 메시지 발행(publish)
 export async function publishEvent(event: Record<string, unknown>) {
   await producer.send({
     topic: "user-events",
